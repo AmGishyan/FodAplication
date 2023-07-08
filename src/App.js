e0e4a1e38f71994b1628ffe54fa990544dac46d1
AmGishyan <arman.656555@gmail.com>
2023-07-08 14:14:46 +0400

    Credit component folder

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Card from './Components/Card';
+import ProductList from 
+
+function App() {
+  return (
+
+ <div className='App'>
+
+   <Header/>
+   
+   <main>
+
+    <ProductList/>
+    <Card/>
+
+   </main>
+  
+</div>
+
+ 
+
+   
+ 
+  );
+}
+
+export default App;
---
+++ src/DummyProducts.js
@@ -0,0 +1,27 @@
+
+
+export const DUMMY_PRODUCTS = [
+    
+    {id:1, name: "Peperoni", category: "Pizza", price: 3000, photo: "images/Peperoni.png"},
+    {id:2, name: "Snkov", category: "Pizza", price: 2600, photo: "images/Peperoni.png"},
+    {id:3, name: "Zeytunov", category: "Pizza", price: 2400, photo: "images/Peperoni.png"},
+    {id:4, name: "Suluguni", category: "Pizza", price: 3200, photo: "images/Peperoni.png"},
+    {id:5, name: "Mocarella", category: "Pizza", price: 3600, photo: "images/Peperoni.png"},
+    {id:6, name: "CocaCola", category: "Drink", price: 300, photo: "images/cola.png"},
+    {id:7, name: "Sprite", category: "Drink", price: 320, photo: "images/cola.png"},
+    {id:8, name: "Fanta", category: "Drink", price: 350, photo: "images/cola.png"},
+    {id:9, name: "Mirinda", category: "Drink", price: 750, photo: "images/cola.png",},
+    {id:10, name: "SevanAnush", category: "Drink", price: 2000, photo: "images/cola.png"},
+    {id:11, name: "J7", category: "Drink", price: 450, photo: "images/cola.png"},
+    {id:12, name: "Xndzor", category: "Fruts", price: 500, photo: "images/frut.png"},
+    {id:13, name: "Narinj", category: "Fruts", price: 600, photo: "images/frut.png"},
+    {id:14, name: "Gilas", category: "Fruts", price: 550, photo: "images/frut.png"},
+    {id:15, name: "Arqayaxndzor", category: "Fruts", price: 300, photo: "images/frut.png"},
+    {id:16, name: "Malina", category: "Fruts", price: 760, photo: "images/frut.png"},
+    {id:17, name: "Adamatuz", category: "Fruts", price: 1200, photo: "images/frut.png"},
+    {id:18, name: "Dambul", category: "Fruts", price: 250, photo: "images/frut.png"},
+    {id:19, name: "Americano", category: "Coffe", price: 1400, photo: "images/coffe.png"},
+    {id:20, name: "Capuchino", category: "Coffe", price: 1200, photo: "images/coffe.png"},
+    {id:21, name: "Glasse", category: "Coffe", price: 1600, photo: "images/coffe.png"}
+    
+]
